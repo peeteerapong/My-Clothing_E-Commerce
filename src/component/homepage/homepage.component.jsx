eac21e720965e303536f1769e475905fa91d7a66
peeteerapong <37838407+peeteerapong@users.noreply.github.com>
2021-04-21 15:26:33 +0700

    first-commint-My-E-Commerce

---
+++ src/component/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../directory/directory.component';
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory/>
+
+    </div>
+
+);
+export default Homepage;
