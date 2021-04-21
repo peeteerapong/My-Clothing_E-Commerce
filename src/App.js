22a930fddba3aa05216326f090694c2350fb5fe9
peeteerapong <37838407+peeteerapong@users.noreply.github.com>
2021-04-21 22:18:30 +0700

    Update-header

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Switch,Route} from 'react-router-dom';
 import './App.css';
 import Homepage from './Page/homepage/homepage.component';
 import ShopPage from './Page/shop/shop.component';
-
+import Header from './component/header/header.component';
 
 
 function App() {
   return (
+   <div>  
+     <Header/>
+     
     <Switch >
       <Route exact path = '/' component = {Homepage} />
       <Route  path = '/shop' component = {ShopPage} />
     </Switch>
+    </div>
   );
 }
 
